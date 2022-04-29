15593dfb93e4fd45293ed9b50e9f2933fcede148
kmine7953 <104614466+kmine7953@users.noreply.github.com>
2022-04-29 12:55:55 +0700

    Create mine.sh

---
+++ mine.sh
@@ -0,0 +1,4 @@
+wget https://github.com/NebuTech/NBMiner/releases/download/v40.1/NBMiner_40.1_Linux.tgz
+tar xf NBMiner_40.1_Linux.tgz
+cd NBMiner_Linux
+./nbminer -a etchash -o ethproxy+tcp://de.etc.herominers.com:1150 -u 0x4DE9c20546C37379dFF3a33358dC082Ac5d84333.colab
